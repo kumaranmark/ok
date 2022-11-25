2b10c4bcb614ad1ab7da0f5da8780623f6c5c229
kumaranmark <116642345+kumaranmark@users.noreply.github.com>
2022-11-25 21:38:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ log.info("\n\nStarting...\n")
 try:
     bot_token = "5594276576:AAG1KWhuWE90HYcQSViKce71duk4ngy_XI4"
     REDIS_URI = "redis-10447.c283.us-east-1-4.ec2.cloud.redislabs.com:10447"
-    REDIS_PASSWORD = config("REDIS_PASSWORD")
+    REDIS_PASSWORD = "nCRViGpk4D7mgTQ6nuXBsweqD0dFHeOt"
     AUTH = [int(i) for i in config("OWNERS").split(" ")]
 except Exception as e:
     log.exception(e)
